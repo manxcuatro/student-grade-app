96985a402537703484d42b31b22bcda5a6321796
Amanda <alphamike.r86@gmail.com>
2026-06-13 14:32:17 +0700

    Initial project stucture

---
+++ grade_functions.py
@@ -0,0 +1,11 @@
+def add_students(students, name, grade):
+    pass
+
+def show_students(students):
+    pass
+
+def find_student(students, name):
+    pass
+
+def average_grade(students):
+    pass
---
+++ main.py
@@ -0,0 +1,8 @@
+from grade_functions import(
+    add_students,
+    show_students,
+    find_student,
+    average_grade
+)
+
+student=[]
