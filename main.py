69c1d01b2a72a12a04a85a4eb2c869477c4ad380
Amanda <alphamike.r86@gmail.com>
2026-06-13 17:11:11 +0700

    integrate all functions

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-from grade_functions import(
-    add_students,
-    show_students,
-    find_student,
-    average_grade
-)
+from grade_functions import *
 
-student=[]
+students = []
+
+while True:
+    print("\n=== MENU ===")
+    print("1. Tambah")
+    print("2. Tampil")
+    print("3. Cari")
+    print("4. Rata-rata")
+    print("5. Keluar")
+
+    pilihan = input("Pilih: ")
+
+    if pilihan == "1":
+        name = input("Nama: ")
+        grade = int(input("Nilai: "))
+        add_student(students, name, grade)
+
+    elif pilihan == "2":
+        show_students(students)
+
+    elif pilihan == "3":
+        name = input("Cari nama: ")
+        print(find_student(students, name))
+
+    elif pilihan == "4":
+        print(average_grade(students))
+
+    elif pilihan == "5":
+        break
